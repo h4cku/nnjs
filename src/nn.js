cd4edbbd926bc16808d4f4f7c3de9851d7031fa5
Elvin <munoz.vega.em@gmail.com>
2024-04-07 08:51:28 +0900

    restructuring the files for npm package

--- src/nn.js
+++ src/nn.js
@@ -576,25 +576,26 @@ class Sequential {
     }
 }
 
-let model = new Sequential([
-    new Linear(2, 3),
-    new Tanh(),
-    new Linear(3, 1)
-])
+export {
+    ActivationFunction,
+    Tensor,
+    Variable,
 
-let x = new Variable(new Tensor([2, 3, 5, 6, 7, 9], [3, 2]), new NopBackward())
-let y = new Variable(new Tensor([3, 5, 7], [3, 1]), new NopBackward())
+    NopBackward,
+    AddBackward,
+    SubBackward,
+    MulBackward,
+    DivBackward,
+    DotBackward,
+    PowBackward,
+    MeanBackward,
+    SigmoidBackward,
+    TanhBackward,
 
-let optim = new SGD(model.parameters(), { lr: 0.1 });
-
-console.log(model.forward(x).val.data)
-let epochs = 10
-for (let epoch = 0; epoch < epochs; epoch++) {
-    optim.zero_grad();
-    let o = model.forward(x);
-    let l = Loss.mse_loss(o, y);
-    console.log(l.val.item());
-    l.backward(new Tensor([1], [1, 1]));
-    optim.step();
+    Loss,
+    SGD,
+    Linear,
+    Sigmoid,
+    Tanh,
+    Sequential
 }
-console.log(model.forward(x).val.data)
